53366d2f8c9625c001aecfc6a0e131d11edeb406
Victor Chung <kindlychung@gmail.com>
2014-06-13 16:56:37 +0200

    wrapped into a python package, register with PyPI

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-packdir=$(pwd)
-packbase=$(basename "$packdir")
-cd ..
-sudo rsync -avu "$packdir" /usr/lib/python2.7/dist-packages/
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = "pygmail2",
+    package = ["pygmail2"],
+    version = "0.1.2",
+    description = "Send mail from gmail using python",
+    author = "Kaiyin Zhong",
+    author_email = "kindlychung@gmail.com",
+    url = "https://github.com/kindlychung/pygmail",
+    keywords = ["gmail", "email"],
+    classifiers = []
+    )
+
